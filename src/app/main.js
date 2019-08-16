906ce9548edbc3b4b7a25106adc780c5895424f3
abiyasa <ilham.suhardi@gmail.com>
2019-08-16 16:30:46 +0200

    hello kontra!

--- src/app/main.js
+++ src/app/main.js
@@ -1,3 +1,28 @@
-import { HELLO } from './hello'
+import { init, Sprite, GameLoop } from 'kontra';
 
-document.write(HELLO)
+const { canvas } = init();
+
+const sprite = Sprite({
+  x: 100,
+  y: 80,
+  color: 'red',
+  width: 20,
+  height: 40,
+  dx: 2
+});
+
+const loop = GameLoop({
+  update() {
+    sprite.update();
+
+    if (sprite.x > canvas.width) {
+      sprite.x = -sprite.width;
+    }
+  },
+
+  render() {
+    sprite.render();
+  }
+});
+
+loop.start();

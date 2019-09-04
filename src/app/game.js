e921521aff4ed604d300a4c7fc2dbb3c304ae23d
abiyasa <ilham.suhardi@gmail.com>
2019-09-04 20:39:59 +0200

    Calculate & shoot bullets properly

--- src/app/game.js
+++ src/app/game.js
@@ -10,44 +10,64 @@ export const GameEngine = {
     initKeys();
     initPointer();
 
-    const enemies = [
+    this.enemies = [
       new Enemy({ color: '#FF8080', x: 320, y: 10 }),
       new Enemy({ color: '#80FF80', x: 160, y: 60 }),
       new Enemy({ color: '#8080FF', x: 480, y: 150 }),
     ]
-    const hero = new Hero({ x: 160, y: 360 });
-    const crosshair = new CrossHair();
-    const bulletPool = Pool({
+    this.hero = new Hero({ x: 160, y: 360 });
+    this.crosshair = new CrossHair();
+    this.bulletPool = Pool({
       create: (props) => new Bullet(props)
     });
 
     GameLoop({
-      update() {
-        crosshair.update();
-        hero.update();
-
-        if (crosshair.canFire()) {
-          crosshair.fire();
-
-          // add bullet
-          bulletPool.get({
-            x: crosshair.sprite.x,
-            y: crosshair.sprite.y,
-            color: 'magenta',
-            ttl: 60
-          });
-        }
-
-        bulletPool.update();
-        enemies.forEach(enemy => enemy.update());
-      },
-
-      render() {
-        hero.render();
-        enemies.forEach(enemy => enemy.render());
-        crosshair.render();
-        bulletPool.render();
-      }
+      update: () => this.update(),
+      render: () => this.render(),
     }).start();
+  },
+
+  update() {
+    const { crosshair, hero, bulletPool, enemies } = this;
+
+    crosshair.update();
+    hero.update();
+
+    if (crosshair.canFire()) {
+      this.shoot();
+    }
+
+    bulletPool.update();
+    enemies.forEach(enemy => enemy.update());
+  },
+
+  render() {
+    this.hero.render();
+    this.enemies.forEach(enemy => enemy.render());
+    this.crosshair.render();
+    this.bulletPool.render();
+  },
+
+  shoot() {
+    const { crosshair, hero, bulletPool } = this;
+    const { x: targetX, y: targetY } = crosshair.sprite;
+    const { x: sourceX, y: sourceY } = hero.sprite;
+
+    crosshair.fire();
+
+    // calculate bullet shooting direction
+    const displacement = { x: targetX - sourceX, y: targetY - sourceY };
+    const distance = Math.sqrt((displacement.x ** 2) + (displacement.y ** 2));
+
+    // add bullet
+    bulletPool.get({
+      x: sourceX,
+      y: sourceY,
+      dx: displacement.x / distance * 10,
+      dy: displacement.y / distance * 10,
+      color: 'magenta',
+      ttl: 60
+    });
   }
+
 };

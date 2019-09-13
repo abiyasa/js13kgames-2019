cdae28a07a570a620bb64c5073d2b86eb1a0c24a
abiyasa <ilham.suhardi@gmail.com>
2019-09-13 12:33:34 +0200

    Show intro asset

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
 import { GameEngine } from './app/game';
+import { loadSVG } from './app/utils';
 import './styles/main.css'
+import assetIntro from './app/intro.svg';
 
-document.querySelector('#button-start').addEventListener('click', () => {
-  document.querySelector('#screen-intro').classList.add('hidden');
-  document.querySelector('#screen-game').classList.remove('hidden');
 
-  startGame();
-});
+async function main() {
+  const assets = await initAssets();
 
-const gameUI = {
-  btnPause: document.querySelector('#button-pause'),
-  labelHeroHp: document.querySelector('#ui-hero-hp'),
-  labelScore: document.querySelector('#ui-score'),
-  panelGameOver: document.querySelector('#panel-game-over'),
-  btnRetry: document.querySelector('#button-retry'),
-};
+  const imageContainerEl = document.querySelector('.filler');
+  imageContainerEl.appendChild(assets.intro);
+
+  document.querySelector('#button-start').addEventListener('click', () => {
+    document.querySelector('#screen-intro').classList.add('hidden');
+    document.querySelector('#screen-game').classList.remove('hidden');
+
+    startGame();
+  });
+}
+
+main();
 
 async function startGame() {
+  const gameUI = {
+    btnPause: document.querySelector('#button-pause'),
+    labelHeroHp: document.querySelector('#ui-hero-hp'),
+    labelScore: document.querySelector('#ui-score'),
+    panelGameOver: document.querySelector('#panel-game-over'),
+    btnRetry: document.querySelector('#button-retry'),
+  };
+
   await GameEngine.init({ gameUI });
   GameEngine.start();
 
@@ -37,4 +49,12 @@ async function startGame() {
     gameUI.btnPause.setAttribute('disabled', true);
     gameUI.panelGameOver.className = '';
   });
+}
+
+async function initAssets() {
+  const intro = await loadSVG(assetIntro);
+
+  return {
+    intro,
+  };
 }
